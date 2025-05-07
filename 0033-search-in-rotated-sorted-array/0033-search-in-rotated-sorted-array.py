f1ed8ca5c6a2a0123f100dd7150e0de7ffab82b6
Ahmed Samir <144508508+a7medsr@users.noreply.github.com>
2025-05-07 18:57:23 +0300

    Time: 0 ms (100%), Space: 18.5 MB (22.77%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -6,17 +6,4 @@ class Solution:
         if target in mp:
             return mp[target]
         return -1
-        nums.sort()
-        l=0
-        r=len(nums)
-        ans=-1
-        while l<=r:
-            mid=(l+r)//2
-            if nums[mid]>target:
-                r=mid-1
-            elif nums[mid]<target:
-                l=mid+1
-            else:
-                ans=1
-
         
